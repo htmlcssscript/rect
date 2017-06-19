2e7b3464903ca3e9b8763410775989a52f311291
htmlcssscript <3239089064@qq.com>
2017-06-19 13:31:48 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(
+	<h1> hello , world</h1>,
+	document.getElementById('root2'));
+registerServiceWorker();
